c83c6e676a8e673ef5aaf078447d7935ebb89d0f
dkrefta <diego.krefta@gmail.com>
2018-03-10 23:29:29 -0300

    Update Code
    Adjust Bootstrap

---
+++ readable-client/src/components/post.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import PropTYpes from 'prop-types';
+
+import {Link, withRouter} from 'react-router-dom';
+
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+import timeago from 'timeago.js';
+import Modal from 'react-modal';
+
+import
