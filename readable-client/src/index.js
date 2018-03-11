a194a14b75ee6cbc2cca7a330e52bfd72d830cf2
dkrefta <diego.krefta@gmail.com>
2018-03-10 21:50:11 -0300

    Update
    New code

--- readable-client/src/index.js
+++ readable-client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from '/reducers'
+
+import {BrowserRouter} from 'react-router-dom';
+
+const reduxDevTools = window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+
+const store = createStore(rootReducer, reduxDevTools)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+      </BrowserRouter>
+    </Provider>, document.getElementById('root'));
+    
 registerServiceWorker();
