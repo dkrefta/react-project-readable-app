2d119d8a95177d4b61c0f25157a686858f15578e
dkrefta <diego.krefta@gmail.com>
2018-03-11 00:55:08 -0300

    Update Code
    new pages

---
+++ readable-client/src/pages/category-page.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {withRouter} from 'react-router-dom';
+
+import {connect} from 'react-redux';
+
+import {getSortedPostsArray} from '../utils/helper';
+import Post from '../components/post';
+import PostForm from '../forms/post-form';
+import SortButtons '../components/sort-buttons';
+
+class CategoryPage extends Component {
+  static PropTypes = {
+    posts: Proptypes.object.isRequired
+  }
+
+  isCategoryAvailable() {
+    const {categories} = this.props;
+    const {category_name} = this.props.match.params;
+    return categories.hasOwnProperty(category_name);
+  }
+
+  generateTitle(category_name) {
+    if(!this.isCategoryAvailable()) {
+      return (
+        <div>
+          <h1> Category incorrect or not right </h1>
+          </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1> All <strong> {category_name.toUpperCase()}</strong> Posts </h1>
+          <SortButtons/>
+          </div>
+      )
+    }
+  }
+
+  render() {
+      const {posts} = this.props
+      const {category_name} = this.props.match.params
+
+      const {sorting} = this.props.prefrences;
+      const postsArray = getSortedPostsArray(posts, sorting);
+      const categoryPosts = postsArray.filter(p => (p.category === category_name));
+
+      return (
+        <div>
+          {this.generateTitle(category_name)}
+          <br/>
+          <ol>
+            {categoryPosts.map((p) => (<Post key={p.id} post={p}/>))}
+          </ol>
+          <div className="addPostContainer">
+            <h3>Add New Post</h3>
+            <PostForm defaultCategory={category_name}/>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  function mapStateToProps({posts, categories, prefrences}) {
+    return {posts, categories, prefrences}
+  }
+
+  export default withRouter(connect(mapStateToProps)(CategoryPage));
