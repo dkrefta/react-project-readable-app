9847bda81664db25822226646bf963a1bd25d11e
dkrefta <diego.krefta@gmail.com>
2018-03-10 23:17:22 -0300

    Update Code
    Update Code

---
+++ readable-client/src/components/navigation-bar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem} from 'react-bootstrap'
+
+import {LinkContainer} from 'react-router-bootstrap'
+
+class NavigationBar extends Component {
+
+  render(){
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to='/'><a>Readable!</a></LinkContainer>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <LinkContainer to="/"><NavItem>Posts</NavItem></LinkContainer>
+                <NavItem href="https://github.com/dkrefta/reactnd-project-readable-starter">Github Repo</NavItem>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+    );
+  };
+}
+
+export default NavigationBar;
